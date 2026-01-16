5e9dda4b16b938f51fe90fb79bcecbd9845484b7
Qamar Zaman <qamarzaman445556@gmail.com>
2026-01-16 11:02:08 -0800

    saylaniassignments

---
+++ JS_Assignment/Calculator/script.js
@@ -0,0 +1,32 @@
+let expression = document.getElementById("expression");
+let result = document.getElementById("result");
+
+function press(value) {
+
+  expression.value += value; 
+
+  }
+function clearAll(){
+    expression.value = expression.value = '';
+    result.value = result.value = '';
+   return;
+  }
+
+
+function del(){
+  expression.value = expression.value.slice(0, -1);
+   
+
+
+}
+
+
+function calculate(){
+    result.value = eval(expression.value);
+
+    
+
+      // result.value = "Error";
+
+}
+
